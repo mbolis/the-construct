db2830492e244911c218cb7aa24a31d7da9e22f5
Marco Bolis <mbolis1984@gmail.com>
2015-11-30 18:12:42 +0100

    On with message dispatcher implementation

--- src/it/mbolis/explore/session/AsyncSession.java
+++ src/it/mbolis/explore/session/AsyncSession.java
@@ -13,97 +13,96 @@ import java.util.function.Consumer;
 
 public class AsyncSession implements Closeable {
 
-	private static final Charset UTF8 = Charset.forName("utf-8");
+    private static final Charset UTF8 = Charset.forName("utf-8");
 
-	private final AsynchronousSocketChannel socketChannel;
-	private final StringBuffer lineBuffer = new StringBuffer();
+    private final AsynchronousSocketChannel socketChannel;
+    private final StringBuffer lineBuffer = new StringBuffer();
 
-	private String name;
+    private String name;
 
-	public AsyncSession(AsynchronousSocketChannel socketChannel) {
-		this.socketChannel = socketChannel;
-	}
+    public AsyncSession(AsynchronousSocketChannel socketChannel) {
+        this.socketChannel = socketChannel;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	private final class OnReadComplete implements CompletionHandler<Integer, ByteBuffer> {
+    private final class OnReadComplete implements CompletionHandler<Integer, ByteBuffer> {
 
-		private final CompletableFuture<String> future;
+        private final CompletableFuture<String> future;
 
-		public OnReadComplete(CompletableFuture<String> future) {
-			this.future = future;
-		}
+        public OnReadComplete(CompletableFuture<String> future) {
+            this.future = future;
+        }
 
-		@Override
-		public void completed(Integer nbytes, ByteBuffer buffer) {
-			if (nbytes == -1) {
-				return;
-			}
-			buffer.flip();
-			CharBuffer chars = UTF8.decode(buffer);
-			lineBuffer.append(chars);
+        @Override
+        public void completed(Integer nbytes, ByteBuffer buffer) {
+            if (nbytes == -1) {
+                return;
+            }
+            buffer.flip();
+            CharBuffer chars = UTF8.decode(buffer);
+            lineBuffer.append(chars);
 
-			int newline = lineBuffer.indexOf("\n");
-			if (newline < 0) {
-				socketChannel.read(buffer, buffer, this);
-				return;
-			}
-			String line = lineBuffer.substring(0, newline);
-			lineBuffer.delete(0, newline + 1);
-			future.complete(line);
-		}
+            int newline = lineBuffer.indexOf("\n");
+            if (newline < 0) {
+                socketChannel.read(buffer, buffer, this);
+                return;
+            }
+            String line = lineBuffer.substring(0, newline);
+            lineBuffer.delete(0, newline + 1);
+            future.complete(line);
+        }
 
-		@Override
-		public void failed(Throwable exc, ByteBuffer buffer) {
-		}
-	}
+        @Override
+        public void failed(Throwable exc, ByteBuffer buffer) {
+        }
+    }
 
-	@FunctionalInterface
-	private static interface FunctionalCompletionHandler<V, A> extends CompletionHandler<V, A> {
+    @FunctionalInterface
+    private static interface FunctionalCompletionHandler<V, A> extends CompletionHandler<V, A> {
 
-		void handle(Throwable error, V result, A attachment);
+        void handle(Throwable error, V result, A attachment);
 
-		@Override
-		default void completed(V result, A attachment) {
-			handle(null, result, attachment);
-		}
+        @Override
+        default void completed(V result, A attachment) {
+            handle(null, result, attachment);
+        }
 
-		@Override
-		default void failed(Throwable exc, A attachment) {
-			handle(exc, null, attachment);
-		}
-	}
+        @Override
+        default void failed(Throwable exc, A attachment) {
+            handle(exc, null, attachment);
+        }
+    }
 
-	public synchronized Future<String> readLine(Consumer<String> callback) {
-		int newline = lineBuffer.indexOf("\n");
-		if (newline >= 0) {
-			String line = lineBuffer.substring(0, newline);
-			lineBuffer.delete(0, newline + 1);
-			callback.accept(line);
-			return CompletableFuture.completedFuture(line);
-		}
+    public synchronized Future<String> readLine(Consumer<String> callback) {
+        int newline = lineBuffer.indexOf("\n");
+        if (newline >= 0) {
+            String line = lineBuffer.substring(0, newline);
+            lineBuffer.delete(0, newline + 1);
+            callback.accept(line);
+            return CompletableFuture.completedFuture(line);
+        }
 
-		ByteBuffer inBuffer = ByteBuffer.allocate(64);
-		CompletableFuture<String> future = new CompletableFuture<>();
-		future.thenAccept(callback);
-		socketChannel.read(inBuffer, inBuffer, new OnReadComplete(future));
-		return future;
-	}
+        ByteBuffer inBuffer = ByteBuffer.allocate(64);
+        CompletableFuture<String> future = new CompletableFuture<>();
+        future.thenAccept(callback);
+        socketChannel.read(inBuffer, inBuffer, new OnReadComplete(future));
+        return future;
+    }
 
-	public synchronized void send(String message) {
-		ByteBuffer bytes = UTF8.encode(message);
-		socketChannel.write(bytes);
-	}
+    public synchronized void send(String message) {
+        ByteBuffer bytes = UTF8.encode(message);
+        socketChannel.write(bytes);
+    }
 
-	public synchronized
-
-	@Override public void close() throws IOException {
-		socketChannel.close();
-	}
+    @Override
+    public void close() throws IOException {
+        socketChannel.close();
+    }
 }
